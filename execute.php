6b82501138da38645391adcc24bad64265d7d46c
Alexandre dos Santos Andrade <alexandretaz@gmail.com>
2016-06-23 16:00:07 -0300

    Mudança na página inicial para mostar os itens enviados

--- execute.php
+++ execute.php
@@ -52,8 +52,36 @@
 
 	unlink('next_rps');
 	file_put_contents ( "next_rps", $lotFile->getNextNumber() );
-	$date = new \Datetime();
-	echo $date->format('d/m/Y as H:i:s')."<br>";
-	echo ' IN ( '. implode(' , ', $arrSalao).')';
-
 ?>
+
+<html>
+	<head>
+		<title> Gerador de RPS </title>
+	</head>
+	<body>
+		<form action="/execute.php" method="post" enctype="multipart/form-data">
+			<label for="upload_rps">
+				<input type="file" id="upload_rps" name="upload_rps">
+				<input type="submit">
+			</label>
+		</form>
+		<h1> Lista de Arquivos de Lote Armazenados no Servidor</h1>
+		<?php
+		$iterator = new FilesystemIterator(FOLDER_TO_UPLOAD, FilesystemIterator::SKIP_DOTS );?>
+
+		<ul> 
+		<?php
+		foreach ($iterator as $item):
+			if( $item->isDir() ):
+				continue;
+			endif;
+		?>
+			<li><a href="/uploads/<?php echo $item->getFileName();?>">
+				<?php $item->getFileName();?>(<?php $item->getMTime();?>)
+			</a> </li>
+		<?php
+		endforeach;
+		?>
+		</ul>
+	</body>
+</html>
